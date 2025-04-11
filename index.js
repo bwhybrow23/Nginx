3118dcd6327970de635a67be8c875a533c3c4690
Ben Whybrow <stentorianbusiness@gmail.com>
2025-04-11 20:53:07 +0000

    Added dynamic Linux support

--- index.js
+++ index.js
@@ -2,45 +2,58 @@
 import fs from 'fs';
 
 //File Imports
-import { ubuntuOS } from './systems/debian';
+import { linuxOS } from './systems/linux';
 // import { windowsOS } from './systems/windows';
 
 //OS => correct file to run
 // eslint-disable-next-line no-undef
 const os = process.platform;
-const distro = fs.readFileSync('/etc/os-release', 'utf8');
 
 switch (os) {
-  case "win32":
+  case "win32": {
     
     //Windows
     // new windowsOS();
     console.log("Currently this doesn't support Windows. The support for NGINX and Certbot isn't great enough to consider it. If you are interested, please contact me.");
 
     break;
+  }
 
-  case "linux":
+  case "linux": {
 
-    //Check if Ubuntu
-    if(distro.includes("Ubuntu") || distro.includes("Debian")) {
-      ubuntuOS();
+    let distro = detectOS();
+    if (distro != 'unknown') {
+      linuxOS(distro)
     } else {
-      console.log("At the moment, this OS is not supported.");
+      console.log("Currently, this Linux distro is not supported. If you think it should be, please open an issue on GitHub.");
     }
+  
+    break;
+  }
 
-  break;
-
-  case "darwin":
+  case "darwin": {
 
     //MacOS
     console.log("Currently this doesn't support MacOS. The support for NGINX and Certbot isn't great enough to consider it. If you are interested, please contact me.");
 
-  break;
+    break;
+  }
 
-  default:
+  default: {
 
     //Unsupported OS
     console.log("Currently this OS is not supported. If you think it should be, please open an issue on GitHub.");
 
     break;
+  }
+}
+
+function detectOS() {
+  const releaseFile = fs.readFileSync('/etc/os-release', 'utf8');
+  if (releaseFile.includes('Debian')) return 'debian';
+  if (releaseFile.includes('Ubuntu')) return 'ubuntu';
+  if (releaseFile.includes('CentOS')) return 'centos';
+  if (releaseFile.includes('Red Hat')) return 'rhel';
+  if (releaseFile.includes('Arch')) return 'arch';
+  return 'unknown';
 }
